5d856f56ce0eff5704ae8a767a5af7629c01cdbd
unknown <tejayalamarthi999@gmail.com>
2024-11-30 18:41:13 +0530

    adding files

---
+++ blood_bank/about-spc.php
@@ -0,0 +1,427 @@
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->
+<!--[if !IE]><!--> <html lang="en"> <!--<![endif]-->
+
+    <head>
+        <meta charset="utf-8">
+        <title>ABOUT BLOOD DONORS DIRECTORY</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <meta name="description" content="Blood Donation - Activism & Campaign HTML5 Template">
+        <meta name="author" content="xenioushk">
+        <link rel="shortcut icon" href="images/favicon.png" />
+
+        <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+        <!--[if lt IE 9]>
+          <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+
+        <!-- The styles -->
+        <link rel="stylesheet" href="css/bootstrap.min.css" />
+        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" >
+        <link href="css/animate.css" rel="stylesheet" type="text/css" >
+        <link href="css/owl.carousel.css" rel="stylesheet" type="text/css" >
+        <link href="css/venobox.css" rel="stylesheet" type="text/css" >
+        <link rel="stylesheet" href="css/styles.css" />
+
+    <body> 
+
+        <div id="preloader">
+            <span class="margin-bottom"><img src="images/loader.gif" alt="" /></span>
+        </div>
+
+        <!--  HEADER -->
+
+        <header class="main-header clearfix" data-sticky_header="true">
+
+            <div class="top-bar clearfix">
+
+                <div class="container">
+
+                    <div class="row">
+
+                        <div class="col-md-4 col-sm-8">
+
+                            <p>Welcome to blood bank directory. </p>
+
+                        </div>
+
+                            <div class="col-md-4 col-sm-4">
+                                    <center>
+                            <p><a href="donar.php"><blink>Need Blood ?</blink></a></p>
+                                </center>
+                        </div>
+                        <div class="col-md-4col-sm-12">
+                            <div class="top-bar-social">
+                                <a href="https://www.facebook.com/SRMUniversityOfficial/"><i class="fa fa-facebook"></i></a>
+                                <a href="mailto:bloodbank@srmist.edu.in" target="_top"><i class="fa fa-envelope "></i></a>
+                               
+                                <a href="https://www.youtube.com/@onlinesrm234"><i class="fa fa-youtube"></i></a>
+                            </div>   
+                        </div> 
+
+                    </div>
+
+                </div> <!--  end .container -->
+
+            </div> <!--  end .top-bar  -->
+
+            <section class="header-wrapper navgiation-wrapper">
+
+                <div class="navbar navbar-default">			
+                    <div class="container">
+
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                            <a class="logo" href="index"><img alt="" src="images/srm_banner.png"></a>
+                        </div>
+
+                        <div class="navbar-collapse collapse">
+                            <ul class="nav navbar-nav navbar-right">
+                                <li class="drop"><a href="index.php" title="Home Layout 01">Home</a></li>
+                                <li><a href="donar.php" title="Donors">Donors</a></li>
+                                <li><a href="be-a-part" title="BE A PART">BE A PART</a></li>
+                                <!-- <li><a href="about-spc" title="About Spc">About Spc</a></li> -->
+                                <li><a href="#about" title="About Us">About Us</a></li>
+                                <li><a href="#about-footer">Contact us</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+            </section>
+
+        </header> <!-- end main-header  -->
+
+        <!--  PAGE HEADING -->
+
+        <section class="page-header" id ="about">
+
+            <div class="container">
+
+                <div class="row">
+
+                    <div class="col-sm-12 text-center">
+
+                        <h3>
+                            ABOUT SPC
+                        </h3>
+
+                        <p class="page-breadcrumb">
+                            <a href="#">Home</a> / ABOUT SPC
+                        </p>
+
+
+                    </div>
+
+                </div> <!-- end .row  -->
+
+            </div> <!-- end .container  -->
+
+        </section> <!-- end .page-header  -->
+
+        <!--  SECTION FAQ -->
+
+        <section class="section-content-block section-faq">
+
+            <div class="container">
+
+                <div class="row">
+
+                    <div class="col-md-12 col-sm-12 text-center">
+                        <h2 class="section-heading">ABOUT US</h2>
+                        <p class="section-subheading">
+                           Know more about SRM  and this project 
+                        </p>
+                    </div> <!-- end .col-sm-10  -->
+
+                </div> <!--  end .row  -->
+
+                <div class="row">
+
+                    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12" id="accordion">
+
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <h4 class="panel-title">
+                                    <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" ><center>ABOUT SRM </center></a>
+                                </h4>
+                            </div>
+                            <div id="collapseOne" class="panel-collapse collapse">
+                                <div class="panel-body">
+                                SRM Institute of Science and Technology  is one of the top ranking universities in India with over 52,000 full time students and more than 3200 faculty across all the campuses – Kattankulathur, Ramapuram, Vadapalani Campus – all in and around Chennai, Tiruchirappalli (in TN), Modinagar (in UP) & Sonepat (in Haryana) – both of which are located near Delhi NCR, Amaravati (in AP), Gangtok (in Sikkim) – offering a wide range of undergraduate, postgraduate and doctoral programs in six Faculties – Engineering & Technology, Management, Medicine & Health sciences, Science & Humanities, Law and Agricultural Sciences.                                </div>
+                            </div>
+                        </div>
+
+                    </div> <!-- end .col-md-6  -->
+
+
+
+                    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12" id="accordion2">
+
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <h4 class="panel-title">
+                                    <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion2" href="#collapseFive" ><center>ABOUT PROJECT</center></a>
+                                </h4>
+                            </div>
+                            <div id="collapseFive" class="panel-collapse collapse">
+                                <div class="panel-body">
+                                •    Is an association between the educational and security frameworks of a state, that trains and encourages youth to develop respect for law and practice abiding by law as a way of life.<br>
+
+                                •   Uses existing network, infrastructure and leadership qualities of Police to supplement physical, mental and educational development of youth.<br>
+
+                                •   Enables school communities to create safe school environments and confident youth willing to react against social evils, and find solutions to community problems.<br>
+
+                                •   Stimulates parents and community leaders to work with police in creating safer communities.<br>
+
+                                </div>
+                            </div>
+                        </div>
+
+                       
+
+                        
+
+                        
+
+                    </div> <!-- end .col-md-12  -->    
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" id="accordion3"> 
+                    <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <h4 class="panel-title">
+                                   <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion3" href="#collapseSeven" ><center>SOCIAL CONTEXT</center> </a>
+                                </h4>
+                            </div>
+                            <div id="collapseSeven" class="panel-collapse collapse">
+                                <div class="panel-body">
+                                   The need for a school-level intervention such as the Student Police Cadet (SPC) project derives from a combination of philosophic, demographic, sociological and economic factors that are expected to influence the future global standing of the nation. The significance of the SPC Project lies in its potential to positively impact the following:<br>
+                                     -  DEMOCRACY AND RESPECT FOR LAW<br>
+                                     -  CHALLENGES OF DEMOCRATIC POLICING<br>
+                                     -  PROBLEMS OF CONTEMPORARY YOUTH<br>
+                                     -  IMPACT OF INFORMATION & COMMUNICATION TECHNOLOGY<br>
+                                    -   DEMOGRAPHIC DIVIDEND<br>
+                                    -   CIVIC SENSE, SOCIAL RESPONSIBILITY AND INCLUSIVENESS.<br>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div> <!-- end .row  -->
+
+            </div> <!-- end .container  -->
+
+
+        </section> <!--  end .section-faq -->
+
+
+        <!-- CLIENT LOGO SECTION  -->
+
+       
+
+        <!-- START FOOTER  -->
+
+   <footer id="about-footer">            
+
+            <section class="footer-widget-area footer-widget-area-bg">
+
+                <div class="container">
+
+                    <div class="row">
+
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+                            <div class="about-footer">
+
+                                <div class="row">
+
+                                    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                                       <!-- <img src="images/logo-footer.png" alt="" /> -->
+                                    </div> <!--  end col-lg-3-->
+
+                                    <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
+                                        <p>
+                                        Students Of SRM Institute of Science and Technology Has Stepped Into The Society By Introducing A Blood Doners Directory In Order To Innovate Every Individual's Participation In These Kind Of Social Activities....
+                                        </p>
+                                    </div> <!--  end .col-lg-9  -->
+
+                                </div> <!--  end .row -->
+
+                            </div> <!--  end .about-footer  -->
+
+                        </div> <!--  end .col-md-12  -->
+
+                    </div> <!--  end .row  -->
+
+                    <div class="row">
+
+                        <div class="col-md-4 col-sm-6 col-xs-12">
+
+                            <div class="footer-widget">
+                                <div class="sidebar-widget-wrapper">
+                                    <div class="footer-widget-header clearfix">
+                                        <h3>Powered by</h3>
+                                    </div>
+                                    <p></p>
+                                    <div class="footer-subscription">
+                                      <li><a href=""><img src="images/srm_logo.jfif" height="45%" width="45%"></a></li>
+                                       
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div> <!--  end .col-md-4 col-sm-12 -->   						                      
+
+                        <div class="col-md-4 col-sm-6 col-xs-12">
+
+                            <div class="footer-widget">
+
+                                <div class="sidebar-widget-wrapper">
+
+                                    <div class="footer-widget-header clearfix">
+                                        <h3>Contact Us</h3>
+                                    </div>  <!--  end .footer-widget-header --> 
+
+
+                                    <div class="textwidget">                                       
+
+                                        <i class="fa fa-envelope-o fa-contact"></i> <p><a href="mail:bloodbank@srmist.edu.in">bloodbank@srmist.edu.in</a></p>
+
+                                        <i class="fa fa-location-arrow fa-contact"></i> <p>Students of<br/>SRM Institute of Science and Technology<br> Chengalpattu <br> Tamil Nadu</p>
+
+                                        <i class="fa fa-phone fa-contact"></i> <p>Phone:&nbsp;  0000000000                           
+
+                                    </div>
+
+                                </div> <!-- end .footer-widget-wrapper  -->
+
+                            </div> <!--  end .footer-widget  -->
+
+                        </div> <!--  end .col-md-4 col-sm-12 -->   
+
+                        <div class="col-md-4 col-sm-12 col-xs-12">
+
+                            <div class="footer-widget clearfix">
+
+                                <div class="sidebar-widget-wrapper">
+
+                                    <div class="footer-widget-header clearfix">
+                                        <h3>Support Links</h3>
+                                    </div>  <!--  end .footer-widget-header --> 
+
+
+                                    <ul class="footer-useful-links">
+
+                                        <li>
+                                            <a href="https://www.facebook.com/SRMUniversityOfficial/">
+                                                <i class="fa fa-caret-right fa-footer"></i>
+                                                Facebook
+                                            </a>
+                                        </li>
+
+                                        <li>
+                                            <a href="https://www.youtube.com/@onlinesrm234">
+                                                <i class="fa fa-caret-right fa-footer"></i>
+                                                Youtube
+                                            </a>
+                                        </li>
+                                             <li>
+                                            <a href="http://studentpolicecadet.org/">
+                                                <i class="fa fa-caret-right fa-footer"></i>
+                                                Student Police Cadet
+                                            </a>
+                                        </li>
+                                        <!-- <li>
+                                            <a href="about-us">
+                                                <i class="fa fa-caret-right fa-footer"></i>
+                                                About Us
+                                            </a>
+                                        </li> -->
+                                        <!-- <li>
+                                            <a href="contact">
+                                                <i class="fa fa-caret-right fa-footer"></i>
+                                                Contact Us
+                                            </a>
+                                        </li> -->
+                                        <li>
+                                            <a href="https://www.srmist.edu.in/">
+                                                <i class="fa fa-caret-right fa-footer"></i>
+                                                SRM
+                                            </a>
+                                        </li>
+
+                                       
+
+                                       
+
+                                        
+
+                                       
+
+                                       
+
+                                                                             
+
+                                    </ul>
+
+                                </div> <!--  end .footer-widget  -->        
+
+                            </div> <!--  end .footer-widget  -->            
+
+                        </div> <!--  end .col-md-4 col-sm-12 -->    
+
+                    </div> <!-- end row  -->
+
+                </div> <!-- end .container  -->
+
+            </section> <!--  end .footer-widget-area  -->
+
+            <!--FOOTER CONTENT  -->
+
+            <section class="footer-contents">
+
+                <div class="container">
+
+                    <div class="row clearfix">
+                             <center>
+                        <div >
+                           
+                            <p > Copyright © Blood Donares Directory <?php echo date('Y');?>. All rights reserved - By <a href='srmist.edu.in'>SRM  </a> </p>
+
+                        </div>  <!-- end .col-sm-6  -->
+                        </center>
+                      
+                        </div> <!--  end .col-lg-6  -->
+
+                    </div> <!-- end .row  -->                                    
+
+                </div> <!--  end .container  -->
+
+            </section> <!--  end .footer-content  -->
+
+        </footer>
+
+        <!-- END FOOTER  -->
+
+        <!-- Back To Top Button  -->
+
+        <a id="backTop">Back To Top</a>
+
+         <script type="text/javascript" src="js/main.js" charset="UTF-8"></script><link rel="stylesheet" crossorigin="anonymous" href="js/main.css"/><script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/wow.min.js"></script>
+        <script src="js/jquery.backTop.min.js "></script>
+        <script src="js/waypoints.min.js"></script>
+        <script src="js/waypoints-sticky.min.js"></script>
+        <script src="js/owl.carousel.min.js"></script>
+        <script src="js/jquery.stellar.min.js"></script>
+        <script src="js/jquery.counterup.min.js"></script>
+        <script src="js/venobox.min.js"></script>
+        <script src="js/custom-scripts.js"></script>
+    </body>
+
+</html>
